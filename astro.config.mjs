50db0671e7c27f4493bad2febb50f8365a522c8d
Personal <martin.milani.mdq@gmail.com>
2024-03-18 13:39:00 -0300

    DUpdated Data

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  image: {
+    domains: ["github.com", "raw.githubusercontent.com","camo.githubusercontent.com","user-images.githubusercontent.com","res.cloudinary.com"],
+  }
 });
