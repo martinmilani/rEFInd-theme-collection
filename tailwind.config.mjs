69156222c2bcb49d3c29ed767d8a91046b544033
Martin Milani <martin.milani.mdq@gmail.com>
2024-12-15 11:45:45 -0300

    fix: solve import error on development

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindDracula from "tailwind-dracula";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwind-dracula")()],
+  plugins: [tailwindDracula()],
 };
