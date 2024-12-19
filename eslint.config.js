e7f181ec8ee9ac80ff5713a08cf450dc93cd35cc
Martin Milani <martin.milani.mdq@gmail.com>
2024-12-19 11:04:35 -0300

    feat: add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended
+];
