ef2826fc54cb4b38ce1c3fa0aeb6cfb923f31a8f
Martin Milani <martin.milani.mdq@gmail.com>
2025-07-17 18:50:03 -0300

    feat: add light-dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,29 @@
 import tailwindDracula from "tailwind-dracula";
 
 export default {
+  darkMode: "class",
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./src/styles/global.css"
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+          950: "#172554",
+        },
+      },
+    },
   },
-  plugins: [tailwindDracula()]
+  plugins: [require("flowbite/plugin"), tailwindDracula()],
 };
